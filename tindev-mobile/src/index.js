df0034128f02c597f098aa6b421d2e5a6274a08f
Lucas Siqueira <lucassiqzro@gmail.com>
2019-08-09 00:35:31 -0300

    Create login page

--- tindev-mobile/src/index.js
+++ tindev-mobile/src/index.js
@@ -1,26 +1,10 @@
 
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+
+import Routes from './routes';
 
 export default function App() {
   return (
-    <View style={sytles.container}>
-      <Text style={sytles.text}>Hello World</Text>
-    </View>
+    <Routes/>
   );
 };
-
-const sytles = StyleSheet.create({
-  container:{
-    flex:1,
-    backgroundColor: '#7159c1',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-
-  text: {
-    fontWeight: 'bold',
-    color: '#FFF',
-    fontSize: 20,
-  }
-})
