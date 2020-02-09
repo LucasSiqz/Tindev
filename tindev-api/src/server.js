add1b94e1bf5d2c6cd6080d7ae5ebad1c4b084ff
Lucas Siqueira <lucassiqzro@gmail.com>
2020-02-09 00:38:33 -0300

    Change mongourl to env variable

--- tindev-api/src/server.js
+++ tindev-api/src/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
 const cors = require('cors');
+const dotenv = require('dotenv');
 
 const app = express();
 const server = require('http').Server(app);
@@ -9,12 +10,14 @@ const io = require('socket.io')(server);
 
 const connectedUsers = {};
 
+dotenv.config();
+
 io.on('connection', socket => {
     const { user } = socket.handshake.query;
     connectedUsers[user] = socket.id;
 });
 
-mongoose.connect('mongodb+srv://lucassiqz:omnistack@cluster0-gupjc.mongodb.net/omnistack8?retryWrites=true&w=majority',{
+mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser:true
 })
 
